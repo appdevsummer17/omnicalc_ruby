1f1ec94a53e22d46685820f178f460c45cf7a64d
Raghu Betina <raghu@firstdraft.com>
2017-07-05 19:36:42 -0500

    Add assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,13 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery
 //= require bootstrap
 //= require turbolinks
 //= require_tree .
+//= require moment
+//= require bootstrap-datetimepicker
+
+$(function () {
+  $('.datetimepicker').datetimepicker();
+});
