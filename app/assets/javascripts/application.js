0ff40b397a6cd14c15279fda0f3818f2d8f247fb
Em Klick <eklick@chicagobooth.edu>
2017-07-12 22:59:22 +0000

    wert

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,13 +10,14 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-//= require jquery
-//= require bootstrap
-//= require turbolinks
-//= require_tree .
-/= require moment
-//= require bootstrap-datetimepicker
+
+require rails-ujs;
+require jquery;
+require bootstrap;
+require turbolinks;
+require_tree;
+require moment;
+require bootstrap-datetimepicker;
 
 $(function () {
   $('.datetimepicker').datetimepicker();
